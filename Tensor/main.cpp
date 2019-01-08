f60a7fca0ac4cc8ddaac48db9b0ef42e6ec39c98
Alexander Bochkarev <otnasa@gmail.com>
2019-01-08 06:41:19 +0400

    add license

--- Tensor/main.cpp
+++ Tensor/main.cpp
@@ -1,9 +1,21 @@
 //
 //  main.cpp
 //  Tensor
+//  A minimal template implementation of n-dimentional array.
 //
-//  Created by Alexander Bochkarev on 13/12/2018.
-//  Copyright © 2018 AB. All rights reserved.
+//  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//  Version 2, December 2004
+//
+//  Copyright (C) 2018 Alexander Bochkarev  <otnasa@gmail.com>
+//
+//  Everyone is permitted to copy and distribute verbatim or modified
+//  copies of this license document, and changing it is allowed as long
+//  as the name is changed.
+//
+//  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
 //
 
 #include <iostream>
