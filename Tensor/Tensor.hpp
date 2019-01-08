ee7d4d2ebebcd6e57b35ac5f97e99ae2371e5c50
Alexander Bochkarev <otnasa@gmail.com>
2019-01-08 06:28:31 +0400

    initial commit

---
+++ Tensor/Tensor.hpp
@@ -0,0 +1,203 @@
+//
+//  Tensor.hpp
+//  Tensor
+//
+//  Created by Alexander Bochkarev on 13/12/2018.
+//  Copyright © 2018 AB. All rights reserved.
+//
+
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+template <class T>
+class Tensor {
+public:
+  /*
+   * Empty constructor initialized empty tensor
+   */
+  Tensor() : begin(nullptr), end(nullptr), value(nullptr), isSubtensor(false)
+  {
+    // do nothing
+  };
+  
+  /*
+   * Constructor with dimensions sizes
+   * data is initialized by default T()
+   */
+  Tensor(const std::vector<int> &dimSizes) : begin(nullptr), end(nullptr), dimSizes(dimSizes),
+    value(nullptr), isSubtensor(false)
+  {
+    if (dimSizes.size() == 0) {
+      value = new T();
+    } else {
+      // create sub-tensor
+      std::vector<int> tail(dimSizes.begin() + 1, dimSizes.end());
+      begin = new Tensor<T>[dimSizes[0]];
+      end = begin + dimSizes[0];
+      for (int i = 0; i < dimSizes[0]; i++) {
+        begin[i] = Tensor(tail);
+      }
+      
+    }
+  }
+  /*
+   * Copy -constructor, should copy entire tensor data
+   */
+  Tensor(const Tensor &rhs)
+  {
+    *this = rhs;
+  }
+  /*
+   * Move -constructor
+   */
+  Tensor(Tensor &&rhs)
+  {
+    begin = rhs.begin;
+    end = rhs.end;
+    rhs.begin = nullptr;
+    rhs.end = nullptr;
+    
+    value = rhs.value;
+    rhs.value = nullptr;
+    
+    isSubtensor = rhs.isSubtensor;
+    
+    // does move
+    dimSizes = std::move(rhs.dimSizes);
+  }
+  
+  /*
+   * Copy -assignment , should copy entire tensor data
+   */
+  Tensor &operator=(const Tensor &rhs)
+  {
+    // does copy
+    dimSizes = rhs.dimSizes;
+    
+    isSubtensor = rhs.isSubtensor;
+    
+    if (rhs.value) { // scalar
+      value = new T();
+      memcpy(value, rhs.value, sizeof(T));
+      begin = nullptr;
+      end = nullptr;
+    } else if (rhs.begin) { // dim > 0, make a deep copy
+      // allocate array
+      begin = new Tensor<T>[dimSizes[0]];
+      end = begin + dimSizes[0];
+      // call copy recursively
+      for (int i = 0; i < dimSizes[0]; i++) {
+        begin[i] = rhs.begin[i];
+      }
+      value = nullptr;
+    }
+    
+    return *this;
+  }
+  
+  /*
+   * Move -assignment
+   */
+  Tensor &operator=(Tensor &&rhs)
+  {
+    std::swap(begin, rhs.begin);
+    rhs.begin = nullptr;
+    std::swap(end, rhs.end);
+    rhs.end = nullptr;
+    std::swap(value, rhs.value);
+    rhs.value = nullptr;
+    std::swap(dimSizes, rhs.dimSizes);
+    
+    isSubtensor = rhs.isSubtensor;
+    
+    return *this;
+  }
+  
+  ~Tensor()
+  {
+    std::cout << "destroy " << std::hex << this << std::endl;
+    if (begin && !isSubtensor) {
+      delete [] begin;
+      begin = nullptr;
+      end = nullptr;
+    }
+    
+    if (value) {
+      delete value;
+      value = nullptr;
+    }
+  }
+
+  /*
+   * Returns dimensions sizes of tensor
+   */
+  std::vector<int> dim() const
+  {
+    return dimSizes;
+  }
+  
+  /*
+   * Returns sub -tensor with indices in first dimension [low ,
+   high)
+   * !!! All changes applied further to sub -tensor would be
+   applied to this tensor as well and vice versa as well ,
+   in other words they would share their content!!!
+   */
+  Tensor operator()(int low , int high) {
+    assert(low >= 0 && high >= 0 && value == nullptr);
+    
+    if (low == high) {
+      return this->operator()(low);
+    }
+    
+    std::vector<int> subDimSizes(dimSizes);
+    subDimSizes[0] = high - low;
+    return Tensor<T>(begin + low, begin + high, subDimSizes);
+  }
+  
+  /*
+   * Returns sub -tensor with index in first dimension pos
+   * !!! All changes applied further to sub -tensor would be
+   applied to this tensor as well and vice versa as well ,
+   in other words they would share their content!!!
+   */
+  Tensor operator()(int pos) {
+    assert(pos >= 0 && value == nullptr);
+    assert(pos < end - begin);
+    // return subtensor
+    return begin[pos];
+  }
+  /*
+   * Returns value indexed by {pos[0], pos[1], pos[2]... pos[
+   dim - 1]}
+   * pos.size() should be equal to dim
+   */
+  T& operator [](std::vector <int> pos) {
+    // pos.size() should be equal to dim
+    assert(pos.size() == dimSizes.size());
+    
+    if (value) {
+      return *value;
+    } else { // dim > 1
+      Tensor subTensor = (*this)(pos[0]);
+      return subTensor[std::vector<int>(pos.begin() + 1, pos.end())];
+    }
+  }
+  
+private:
+  Tensor(Tensor<T> *begin, Tensor<T> *end, const std::vector<int> &dimSizes) : begin(begin), end(end)
+  {
+    isSubtensor = true;
+    this->dimSizes = std::move(dimSizes);
+  }
+  
+  std::vector<int> dimSizes;
+  // tensor data
+  Tensor<T> *begin;
+  Tensor<T> *end;
+  
+  T *value;
+  bool isSubtensor;
+};
---
+++ Tensor/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Tensor
+//
+//  Created by Alexander Bochkarev on 13/12/2018.
+//  Copyright © 2018 AB. All rights reserved.
+//
+
+#include <iostream>
+#include "Tensor.hpp"
+
+int main(int argc, const char * argv[]) {
+
+  Tensor<float> t1 = Tensor<float>({5, 5, 5}); // create new tensor with shape 5x5x5
+  Tensor<float> t2 = t1(3, 4); // shape 1x5x5, shares data with t1
+  Tensor<float> t3 = t2; // shape 1x5x5, doesn’t share any data with t1 and t2 as copied
+  Tensor<float> t4; // empty tensor
+  t4 = t1(3); // shape 5x5, shares data with t1 and t2, move semantics here
+  Tensor<float> t5 = t1(3)(4)(2, 5); // shape 3, shares data with t1, t2, t4
+  t5[{0}] = 100.0; // t1[{3, 4, 3}], t2[{0, 4, 3}], t4[{4, 3}] are 100.0 as well, but t3[{0, 4, 3}] is 0.0
+
+  float t1_343 = t1[{3, 4, 3}];
+  float t2_043 = t2[{0, 4, 3}];
+  assert(t1_343 == t2_043);
+
+  t1[{3, 4, 4}] = -100.0; // t2[{0, 4, 4}], t4[{4, 4}], t5[{2}] are -100.0 as well, but t3[{0, 4, 4}] is 0.0
+
+  float t2_044 = t2[{0, 4, 4}];
+  float t4_44 = t4[{4, 4}];
+  assert(t2_044 == t4_44);
+  
+  std::cout << "all works fine" << std::endl;
+
+  return 0;
+}
